550f029a0bcc0a30bfb3f8bcd3cf4d215639e8e5
AD25 <angelo251158@gmail.com>
2022-12-20 13:07:59 +0100

    create

---
+++ database/factories/UserinfoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Userinfo;
+use Faker\Generator as Faker;
+
+$factory->define(Userinfo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
